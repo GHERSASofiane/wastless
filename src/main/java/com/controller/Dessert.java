39cd8ffb5a72e15af8ab4b4cf9d6bea098894ab1
Sofiane GHERSA <m.ghersa.s@gmail.com>
2018-10-06 11:20:57 +0200

    Add BDD

--- src/main/java/com/controller/Dessert.java
+++ src/main/java/com/controller/Dessert.java
@@ -35,12 +35,7 @@ public class Dessert extends HttpServlet {
 		try {
 			Connection db = connection.getConnection();
 			Statement stmt = db.createStatement();
-			stmt.executeUpdate("CREATE TABLE Booking ( 
-				BookingId int(11) NOT NULL,
-				BookingDated varchar(15) NOT NULL,
-				ProductId int(11) NOT NULL,
-				UserId int(11) NOT NULL
-			  )");
+			stmt.executeUpdate("CREATE TABLE Booking (BookingId int(11) NOT NULL, BookingDated varchar(15) NOT NULL, ProductId int(11) NOT NULL, UserId int(11) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=latin1");
 	        // stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
 	        // stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
 		 

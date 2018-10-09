79a7732783502afafee2c2d02f5487fe3fd1984a
Sofiane GHERSA <m.ghersa.s@gmail.com>
2018-10-09 09:44:10 +0200

    add Inscrition

--- src/main/java/com/controller/signIn.java
+++ src/main/java/com/controller/signIn.java
@@ -1,7 +1,11 @@
 package com.controller;
 
 import java.io.IOException;
-import javax.servlet.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 public class signIn  extends HttpServlet {

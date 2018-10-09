05fd885ed6b082d9813c83e481df5ba0a038fcab
naim <naim.choullit0504@gmail.com>
2018-10-09 19:05:48 +0200

    firt commit for naim

--- src/main/java/controller/SignIn.java
+++ src/main/java/controller/SignIn.java
@@ -31,6 +31,14 @@ public class SignIn extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		 response.setContentType("application/json");
+		 response.setHeader("Access-Control-Allow-Origin", "*"); 
+		 response.setHeader("Access-Control-Allow-Credentials", "true");
+		 response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+		 response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+	    
+		
 		PrintWriter pw = response.getWriter();
 		pw.println(JSonConverter.objectToJson(new Reponse("ok", "")));
 		pw.flush();

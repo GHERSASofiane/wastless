46bc84496e408ee7b1a5d8393c4f75b2f9982d81
Sofiane GHERSA <m.ghersa.s@gmail.com>
2018-10-10 15:18:46 +0200

    Add dependence gson

--- src/main/java/com/controller/OfferSearch.java
+++ src/main/java/com/controller/OfferSearch.java
@@ -6,11 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.zetcode.bean.City;
+ 
 
 public class OfferSearch  extends HttpServlet {
     

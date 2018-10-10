cf7c0765a50c74eedb94bce045ff4196febc5787
Sofiane GHERSA <m.ghersa.s@gmail.com>
2018-10-10 15:15:26 +0200

    Add OfferSearch web.xml

--- src/main/java/com/controller/OfferSearch.java
+++ src/main/java/com/controller/OfferSearch.java
@@ -6,7 +6,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.zetcode.bean.City;
 
 public class OfferSearch  extends HttpServlet {
     

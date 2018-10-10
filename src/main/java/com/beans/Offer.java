12123af392b4c33f3fd7ee1d364c0faece822149
Sofiane GHERSA <m.ghersa.s@gmail.com>
2018-10-10 14:57:46 +0200

    Add OfferSearch model

---
+++ src/main/java/com/beans/Offer.java
@@ -0,0 +1,8 @@
+package com.beans;
+
+
+public class Offer {
+    
+
+
+}
--- src/main/java/com/controller/OfferSearch.java
+++ src/main/java/com/controller/OfferSearch.java
@@ -13,7 +13,26 @@ public class OfferSearch  extends HttpServlet {
     public OfferSearch() { }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
 
+        response.setContentType("text/html");
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("<HTML>\n");
+        sb.append("<HEAD>\n");
+        sb.append("<TITLE>Bonjour</TITLE>\n");
+        sb.append("</HEAD>\n");
+        sb.append("<BODY>\n");
+        sb.append("<H1>Bonjour</H1>\n");
+        sb.append("</BODY>\n");
+        sb.append("</HTML>");
+        response.setContentLength(sb.length());
+
+        // envoi de la réponse
+        response.getOutputStream().print(sb.toString());
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

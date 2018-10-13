ba147c64a5fd39b8c1f32209695f523d03cacb58
Sofiane GHERSA <m.ghersa.s@gmail.com>
2018-10-13 11:51:45 +0200

    Add ORDRE BY ProductDate

--- src/main/java/com/model/EnsembleOffer.java
+++ src/main/java/com/model/EnsembleOffer.java
@@ -41,7 +41,7 @@ public class EnsembleOffer {
 
         Connection db = connection.getConnection();
         Statement stmt = db.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM Product, Users WHERE ProductStatus = 0 AND Product.UserId = Users.UserId AND ProductName LIKE '%"+this.nameArticle+"%' OFFSET "+(this.page*10)+" LIMIT 10 ORDER BY ProductDate");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM Product, Users WHERE ProductStatus = 0 AND Product.UserId = Users.UserId AND ProductName LIKE '%"+this.nameArticle+"%' ORDER BY ProductDate OFFSET "+(this.page*10)+" LIMIT 10 ");
         while (rs.next()) {
             ProductName = rs.getString("ProductName");
             ProductDate = rs.getString("ProductDate");

39dcb928d1abe2ee6198f83d390e5ea6240d5c30
Sofiane GHERSA <m.ghersa.s@gmail.com>
2018-10-23 14:01:59 +0200

    AddProduct  Verifie params

--- src/main/java/dao/ProductDAO.java
+++ src/main/java/dao/ProductDAO.java
@@ -60,8 +60,8 @@ public class ProductDAO {
 
     public Reponse addProduct(Product product) {
 
-    	product.setProductDescription(product.getProductDescription().toLowerCase()); 
-    	product.setProductName(product.getProductName().toLowerCase());
+    	product.setProductDescription("product.getProductDescription().toLowerCase()"); 
+    	product.setProductName("product.getProductName().toLowerCase()");
 //    	product.setProductDate(getDate());
 //    	product.setProductStatus(0);
 //        String ProductName, 
